0c6033e092aab19911ffd5579fd7c086a9cbeae8
Anna <anna4776497@gmail.com>
2022-10-26 07:37:23 +0300

    дошла до провайдера в APP, смотрим что дальше

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css';
-import App from './conponents/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./conponents/App/App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-      </BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
-// import { createStore } from "redux";
+import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import clickOrder from '../reducers/clickOrder';
+import * as actions from '../actions/actions'
 
-// const store = createStore(reducer);
+const store = createStore(clickOrder);
+ 
+//const {dispatch, subscribe, getState} = store;
 
-// export default store;
+export default store;
